754407bba9639318940fa5b06946d29eb1cd86a8
flowadver <169107808+flowadver@users.noreply.github.com>
2024-06-09 16:57:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function shrinkImage() {
     var img = document.getElementById("myImage");
-    img.style.width = "50%"; // Atur ukuran gambar menjadi 50% dari ukuran awal
+    img.style.width = "35%"; 
 }
